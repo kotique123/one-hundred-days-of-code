78869b89e3318a7e70aaa9f7d31e07ad9fd75017
Daniil Murashov <DaniilMurashov@icloud.com>
2025-01-09 16:06:31 -0600

    Added comments to Day-8 solution

--- Day-9/largest_series_product.go
+++ Day-9/largest_series_product.go
@@ -1,3 +1,5 @@
+// Package lsproduct is a small library that has allow to determine the largest
+// product in a series of numbers.
 package lsproduct
 
 import (
@@ -5,17 +7,24 @@ import (
 	"strconv"
 )
 
+// LargestSeriesProduct accepts a series of numbers as a string and a span of
+// series and returns sum and error.
 func LargestSeriesProduct(digits string, span int) (int64, error) {
+	// checks if span is negative
 	if span < 0 {
 		return 0, errors.New("span must not be negative")
+		// chekcs if span is longer than series
 	} else if span > len(digits) {
 		return 0, errors.New("span must be smaller than string length")
 	}
+	// allocates memory for a slice for numbers from a string
 	nums := make([]int, len(digits))
 	for i := 0; i < len(digits); i++ {
 		num, err := strconv.Atoi(string(digits[i]))
 		if err == nil {
 			nums[i] = num
+			// if a sequence is not valid, meaning that there are values other than
+			// numerical, returns an error
 		} else {
 			return 0, errors.New("digits input must only contain digits")
 		}
